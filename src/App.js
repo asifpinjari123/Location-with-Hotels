503e0bc8588831947c42ff11f896d12b541cec12
asif <asifmansuri736@gmail.com>
2023-09-27 12:39:42 +0530

    submited code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from "react";
+
+const Hotels = [
+  {
+    location: "Nashik",
+    hotels: [
+      "Holiday Inn Express Nashik Indira Nagar, an IHG Hotel 3 stars",
+      "ibis Nashik - An Accor Brand",
+      "Click Hotel Nashik "
+    ]
+  },
+  {
+    location: "Malegaon",
+    hotels: ["Apsara", "Hotel Radhika", "Hotel City Place"]
+  },
+  {
+    location: "Mumbai",
+    hotels: [
+      "The Orchid Hotel Mumbai Vile Parle",
+      "The Taj Mahal Tower, Mumbai",
+      "Trident Nariman Point "
+    ]
+  }
+];
 
 function App() {
+  const [selectLocation, setSelectLocation] = useState("");
+  const [hotelsList, setHoteslList] = useState("");
+
+  const handleLocation = (e) => {
+    setSelectLocation(e.target.value);
+  };
+
+  const handleHotels = (e) => {
+    setHoteslList(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Select a Location:</label>
+      <select onChange={handleLocation} value={selectLocation}>
+        <option value="">Select a Location</option>
+        {Hotels.map((item) => (
+          <option key={item.location} value={item.location}>
+            {item.location}
+          </option>
+        ))}
+      </select>
+
+      {selectLocation && (
+        <div>
+          <label>Choose Hotels:</label>
+          <select onChange={handleHotels} value={hotelsList}>
+            <option value="">Select a Hotels</option>
+            {Hotels.find((item) => item.location === selectLocation).hotels.map(
+              (hotel) => (
+                <option key={hotel} value={hotel}>
+                  {hotel}
+                </option>
+              )
+            )}
+          </select>
+        </div>
+      )}
+
+      {hotelsList && (
+        <div>
+          <p>Selected Location: {selectLocation}</p>
+          <p>Selected Hotels: {hotelsList}</p>
+        </div>
+      )}
     </div>
   );
 }
